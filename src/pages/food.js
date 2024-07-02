105ec8c000bc05eb7cd601ff9e998a477e8a22bd
Nathan Doan <nathandoan24@gmail.com>
2024-07-03 07:02:13 +1200

    Part 3

---
+++ src/pages/food.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const FoodPage = () => {
+  return (
+    <Layout pageTitle="Favorite Foods">
+      <p></p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>Food Me</title>
+
+export default FoodPage
