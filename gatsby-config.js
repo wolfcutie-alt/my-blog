50e796e7569abc8629c02bbfa3f90f7ad0a43ff1
Nathan Doan <nathandoan24@gmail.com>
2024-07-04 20:42:56 +1200

    Finish

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,5 +16,7 @@ module.exports = {
         path: `${__dirname}/blog`,
       }
     },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
   ],
 }
---
+++ src/pages/blog/index.js
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
+import { graphql, Link } from 'gatsby'
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <ul>
+      {
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
+        ))
+      }
+      </ul>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+          slug
+        }
+        id
+        excerpt
+      }
+    }
+  }
+`
+
+export const Head = () => <Seo title="My Blog Posts" />
+
+export default BlogPage
